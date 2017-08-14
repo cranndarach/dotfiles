0ec4c9fdd1e7dc580446859bac9436186ea7f8f5
R Steiner <r.steiner@uconn.edu>
2017-08-14 15:13:11 -0400

    Make brightness an int.

--- .config/powerline/segments/common/brightness.py
+++ .config/powerline/segments/common/brightness.py
@@ -5,5 +5,6 @@ from powerline.lib.shell import run_cmd
 
 def brightness(pl):
     '''Return the monitor's brightness according to xbacklight.'''
-    brightness = round(float(run_cmd(pl, ["xbacklight", "-get"])), 0)
+    # It doesn't want to go right from float to int.
+    brightness = int(round(float(run_cmd(pl, ["xbacklight", "-get"])), 0))
     return "{}%".format(str(brightness))
